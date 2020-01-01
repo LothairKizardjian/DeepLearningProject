1815aa7f800cfef64a939e55cefcd344b858d99a
LothairKizardjian <lothair.kizardjian@laposte.net>
2020-01-01 13:31:38 +0100

    models

--- golois.py
+++ golois.py
@@ -1,14 +1,17 @@
 import tensorflow as tf
 import tensorflow.keras as keras
+import tensorflow.keras.regularizers as regularizers
 import numpy as np
+import os
 from tensorflow.keras import layers 
 
-def residual_layer(input, filters, filter_size):
-    ident = layers.Conv2D(filters, 1, padding='same')(input)    
+os.environ["CUDA_VISIBLE_DEVICES"] = "1" 
+
+def residual_layer(input, filters, filter_size):    
     res_layer = convolutional_layer(input, filters, filter_size)
     res_layer = layers.Conv2D(filters, filter_size, padding='same')(res_layer)
     res_layer = layers.BatchNormalization()(res_layer)
-    res_layer = layers.add([ident,res_layer])
+    res_layer = layers.add([input,res_layer])
     res_layer = layers.Activation('relu')(res_layer)
     return res_layer
 
@@ -19,8 +22,8 @@ def convolutional_layer(input, filters, filter_size):
     return conv_layer
 
 def get_model():
-    res_layers = 11
-    filters = 64
+    res_layers = 4
+    filters = 256
     filter_size = 3
     input = layers.Input(shape=(19, 19, planes), name='board')    
     for i in range(res_layers):
@@ -41,6 +44,77 @@ def get_model():
 
     return model
 
+def build_model(input_shape, moves, blocks, filters, value_dense):
+    input = layers.Input(shape=input_shape, name="board")
+    x = layers.Conv2D(
+        filters=filters,
+        kernel_size=(3,3),
+        strides=(1,1),
+        padding="same",
+        activation=None,
+        use_bias=False,
+        kernel_regularizer=regularizers.l2(1e-4))(input)
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation("relu")(x)
+    x = layers.Dropout(0.2)(x)
+
+    for i in range(blocks):
+        copy = x
+        x = layers.Conv2D(
+            filters=filters,
+            kernel_size=(3,3),
+            strides=(1,1),
+            padding="same",
+            activation=None,
+            use_bias=False,
+            kernel_regularizer=regularizers.l2(1e-4))(x)
+        x = layers.BatchNormalization()(x)
+        x = layers.Activation("relu")(x)
+
+        x = layers.Conv2D(filters=filters,
+                          kernel_size=(3,3),
+                          strides=(1,1),
+                          padding="same",
+                          activation=None,
+                          use_bias=False,
+                          kernel_regularizer=regularizers.l2(1e-4))(x)
+        x = layers.BatchNormalization()(x)
+        x = layers.Add()([x, copy])
+        x = layers.Activation("relu")(x)
+
+    policy = layers.Conv2D(
+        filters=2,
+        kernel_size=(1,1),
+        strides=(1,1),
+        padding="same",
+        activation=None,
+        use_bias=False,
+        kernel_regularizer=regularizers.l2(1e-4))(x)
+    policy = layers.BatchNormalization()(policy)
+    policy = layers.Activation("relu")(policy)
+    policy = layers.Flatten()(policy)
+    policy = layers.Dropout(0.2)(policy)
+    policy = layers.Dense(moves, activation="softmax", name="policy")(policy)
+
+    value = layers.Conv2D(
+        filters=1,
+        kernel_size=(1, 1),
+        strides=(1,1),
+        padding="same",
+        activation=None,
+        use_bias=False,
+        kernel_regularizer=regularizers.l2(1e-4))(x)
+    value = layers.BatchNormalization()(value)
+    value = layers.Activation("relu")(value)
+    value = layers.Flatten()(value)
+    value = layers.Dropout(0.4)(value)
+    value = layers.Dense(value_dense)(value)
+    value = layers.Dense(1, activation="tanh", name="value")(value)
+
+    model = keras.Model(inputs=input, outputs=[policy,value])
+    model.summary()
+    return model
+
 def train(model,model_title,epochs,batch_size):
     model.compile(optimizer=keras.optimizers.SGD(lr=0.1),
                   loss={'value': 'mse', 'policy': 'categorical_crossentropy'})
@@ -54,7 +128,7 @@ def train(model,model_title,epochs,batch_size):
 
 planes = 8
 moves = 361
-dynamicBatch = False
+dynamicBatch = True
 if dynamicBatch:
     import golois
     N = 100000
@@ -76,7 +150,7 @@ else:
     policy = np.load ('policy.npy')
     value = np.load ('value.npy')
     end = np.load ('end.npy')
-
-model = get_model()
-train(model,'LK_ResGo_v1',100,64)
+    
+model = build_model((19,19,planes),361,10,62,64)
+train(model,'LK_ResGo_v4',50,64)
     
