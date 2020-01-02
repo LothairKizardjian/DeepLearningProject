af478e076081c1fcc464f4aa815aebc8a651058f
Lothair Kizardjian <lothair.kizardjian@laposte.net>
2020-01-02 19:36:57 +0100

    modified compiler to Adadelta

--- golois.py
+++ golois.py
@@ -7,43 +7,6 @@ from tensorflow.keras import layers
 
 os.environ["CUDA_VISIBLE_DEVICES"] = "1" 
 
-def residual_layer(input, filters, filter_size):    
-    res_layer = convolutional_layer(input, filters, filter_size)
-    res_layer = layers.Conv2D(filters, filter_size, padding='same')(res_layer)
-    res_layer = layers.BatchNormalization()(res_layer)
-    res_layer = layers.add([input,res_layer])
-    res_layer = layers.Activation('relu')(res_layer)
-    return res_layer
-
-def convolutional_layer(input, filters, filter_size):
-    conv_layer = layers.Conv2D(filters, filter_size, padding='same')(input)
-    conv_layer = layers.BatchNormalization()(conv_layer)
-    conv_layer = layers.Activation('relu')(conv_layer)
-    return conv_layer
-
-def get_model():
-    res_layers = 4
-    filters = 256
-    filter_size = 3
-    input = layers.Input(shape=(19, 19, planes), name='board')    
-    for i in range(res_layers):
-        if i==0:
-            x = residual_layer(input, filters, filter_size)
-        else:
-            x = residual_layer(x, filters, filter_size)
-        
-    policy_head = layers.Conv2D(1, 3, activation='relu', padding='same')(x)
-    policy_head = layers.Flatten()(policy_head)
-    policy_head = layers.Dense(moves, activation='softmax', name='policy')(policy_head)
-    
-    value_head = layers.Flatten()(x)
-    value_head = layers.Dense(1, activation='sigmoid', name='value')(value_head)
-
-    model = keras.Model(inputs=input, outputs=[policy_head, value_head])
-    model.summary ()
-
-    return model
-
 def build_model(input_shape, moves, blocks, filters, value_dense):
     input = layers.Input(shape=input_shape, name="board")
     x = layers.Conv2D(
@@ -93,7 +56,6 @@ def build_model(input_shape, moves, blocks, filters, value_dense):
     policy = layers.BatchNormalization()(policy)
     policy = layers.Activation("relu")(policy)
     policy = layers.Flatten()(policy)
-    policy = layers.Dropout(0.2)(policy)
     policy = layers.Dense(moves, activation="softmax", name="policy")(policy)
 
     value = layers.Conv2D(
@@ -107,7 +69,6 @@ def build_model(input_shape, moves, blocks, filters, value_dense):
     value = layers.BatchNormalization()(value)
     value = layers.Activation("relu")(value)
     value = layers.Flatten()(value)
-    value = layers.Dropout(0.4)(value)
     value = layers.Dense(value_dense)(value)
     value = layers.Dense(1, activation="tanh", name="value")(value)
 
@@ -116,8 +77,10 @@ def build_model(input_shape, moves, blocks, filters, value_dense):
     return model
 
 def train(model,model_title,epochs,batch_size):
-    model.compile(optimizer=keras.optimizers.SGD(lr=0.1),
-                  loss={'value': 'mse', 'policy': 'categorical_crossentropy'})
+    model.compile(optimizer=tf.keras.optimizers.Adadelta(learning_rate=1),
+                  loss={'value': 'mse', 'policy': 'categorical_crossentropy'},
+                  metrics=['accuracy']
+    )
 
     checkpointer  = keras.callbacks.ModelCheckpoint(filepath='./models/{}.h5'.format(model_title),
                                                     verbose=1,
@@ -129,6 +92,7 @@ def train(model,model_title,epochs,batch_size):
 planes = 8
 moves = 361
 dynamicBatch = True
+
 if dynamicBatch:
     import golois
     N = 100000
